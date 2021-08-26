4486356cecd2cc368d61c504f910fecfc5deb298
Bryan Lee <>
2021-08-26 14:30:47 -0400

    cart viewcontroller and navbar

---
+++ iTunesProject/CartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CartViewController.swift
+//  iTunesProject
+//
+//  Created by Bryan Lee on 8/26/21.
+//
+
+import UIKit
+
+class CartViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Cart"
+        configNavBar()
+        
+    }
+    
+}
+
+extension CartViewController {
+    private func configNavBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Buy", style: .plain, target: self, action: nil)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Browse", style: .plain, target: self, action: nil)
+    }
+}
+    
+    
+
