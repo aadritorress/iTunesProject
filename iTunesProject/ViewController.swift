8810a1b3e31d1a2e31043f8d8d7d30d2f93a829e
Adri Torres <adriana.amaral@msn.com>
2021-08-26 13:15:42 -0400

    navigation-controller

--- iTunesProject/ViewController.swift
+++ iTunesProject/ViewController.swift
@@ -12,12 +12,24 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
+        title = "Browser"
         
-        
-        
-        
+        configItunes()
     }
 
-
+    private func configItunes() {
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(
+//            barButtonSystemItem: .camera, target: self, action: nil
+//        )
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "cart"), style: .plain, target: self, action: nil)
+        
+//        navigationItem.leftBarButtonItem = UIBarButtonItem(
+//            barButtonSystemItem: .play, target: self, action: nil
+//        )
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "music.note.list"), style: .plain, target: self, action: nil)
+    }
 }
+
 
